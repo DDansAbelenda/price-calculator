448caf6f889f47b5fd872dcd91c4bf2ff6db524f
Daniel Dans Abelenda <ddansabelenda@gmail.com>
2024-07-22 21:00:10 -0400

    agregando vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
 import ReactDOM from 'react-dom/client'
 import AppRoutes from "./routes";
 import GlobalContextProvider from "./context/GlobalContext";
+import { Analytics } from "@vercel/analytics/react"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <>
     <GlobalContextProvider>
       <AppRoutes />
     </GlobalContextProvider>
+    <Analytics />
   </>
 )
